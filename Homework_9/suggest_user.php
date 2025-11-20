4b98cfa86e351e79d1cc18a8ef8b95b1d1f06fce
Konark <kkonark@constructor.university>
2025-11-20 03:50:16 +0100

    hw9 updated

--- Homework_9/suggest_user.php
+++ Homework_9/suggest_user.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 header("Content-Type: application/json");
 require_once "../Homework_6/db_connect.php";  // correct path for your structure
 
